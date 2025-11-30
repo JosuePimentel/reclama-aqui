8f6937a47e227dfe456d1f7e987faf427fdcdb5d
Josué Farias Pimentel <josue.farias.pimentel@gmail.com>
2025-11-30 16:26:06 -0300

    feat: arruma prod

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from "./app.js";
 import { env } from "@env";
 
 app
-  .listen({ port: env!.PORT })
+  .listen({ port: env!.PORT, host: env!.ENVIRONMENT !== 'dev' ? '0.0.0.0' : 'localhost' })
   .then(() => console.log(`HTTP SERVER Running in port ${env!.PORT}!`))
